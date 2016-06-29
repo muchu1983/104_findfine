cfad431a60a9216eea2e694ce251902293b8bec5
MuChu Hsu <muchu1983@gmail.com>
2016-06-29 18:33:22 +0800

    home find btn call trip/filter

--- findfine/static/js/home.js
+++ findfine/static/js/home.js
@@ -1,1 +1,33 @@
+/*
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
 
+<https://opensource.org/licenses/BSD-3-Clause>
+*/
+(function($){
+    
+    $(document).ready(initHome);
+    
+    function initHome(){
+        $("#btnFindTrip").click(function(){
+            var strFilterText = $("#strFilterText").val()
+            $.getJSON("/trip/filter", function(jsonResp){
+                console.log(jsonResp);
+                for (i = 0; i < jsonResp.length; i++) {
+                    var dicTripData = jsonResp[i];
+                    var strTripDataHtml = [
+                        "<li>",
+                            "<div class=\"tripData\">",
+                                "<span>" + dicTripData["strTitle"] + "</span>",
+                            "</div>",
+                        "</li>"
+                    ].join("");
+                    $("div.findResultDiv ul.lstTripData").append(strTripDataHtml);
+                };
+                $("div.findResultDiv").fadeIn();
+            });
+        });
+    }
+    
+})(jQuery);
