e792a7daa17e9fc217400a507391ab9d141cf1ff
MuChu Hsu <muchu1983@gmail.com>
2016-06-29 19:41:08 +0800

    arrange trip card

--- findfine/static/js/home.js
+++ findfine/static/js/home.js
@@ -11,23 +11,46 @@ This file is part of BSD license
     
     function initHome(){
         $("#btnFindTrip").click(function(){
-            var strFilterText = $("#strFilterText").val()
-            $.getJSON("/trip/filter", function(jsonResp){
+            var strFilterText = $("#strFilterText").val();
+            var strFilterQueryUrl = "/trip/filter";
+            if (strFilterText != ""){
+                strFilterQueryUrl = strFilterQueryUrl + "?keyword=" + strFilterText;
+            };
+            $.getJSON(strFilterQueryUrl, function(jsonResp){
                 console.log(jsonResp);
+                $("div.findResultDiv ul.lstTripData").html("")
                 for (i = 0; i < jsonResp.length; i++) {
                     var dicTripData = jsonResp[i];
-                    var strTripDataHtml = [
-                        "<li>",
-                            "<div class=\"tripData\">",
-                                "<span>" + dicTripData["strTitle"] + "</span>",
-                            "</div>",
-                        "</li>"
-                    ].join("");
+                    var strTripDataHtml = getTripDataHtml(dicTripData["strTitle"], dicTripData["intUsdCost"], dicTripData["strIntroduction"]);
                     $("div.findResultDiv ul.lstTripData").append(strTripDataHtml);
                 };
                 $("div.findResultDiv").fadeIn();
+                $("#btnHideTrip").show();
             });
         });
-    }
+        
+        $("#btnHideTrip").click(function(){
+            $("div.findResultDiv").fadeOut();
+            $("#btnHideTrip").hide();
+        });
+    };
+    
+    function getTripDataHtml(strTitle, intUsdCost, strIntroduction){
+        var strTripDataHtml = [
+            "<li>",
+            "<div class=\"tripData\">",
+                "<div class=\"tripImgDiv\"><img src=\"/static/img/TripCard.png\"/></div>",
+                "<div class=\"tripTitleDiv\">",
+                    "<span class=\"tripTitleSpan\">" + strTitle + "</span>",
+                    "<span class=\"tripUsdCostSpan\">" + intUsdCost + " USD</span>",
+                "</div>",
+                "<div class=\"tripIntroDiv\">",
+                    "<span>" + strIntroduction + "</span>",
+                "</div>",
+            "</div>",
+            "</li>"
+        ].join("");
+        return strTripDataHtml;
+    };
     
 })(jQuery);
