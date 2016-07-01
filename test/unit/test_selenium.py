871b515b7b44a48e0494bdcc757f4cecd5afa823
MuChu Hsu <muchu1983@gmail.com>
2016-07-01 18:39:30 +0800

    rename crawler to findfine_crawler SeleniumTest okay

--- test/unit/test_selenium.py
+++ test/unit/test_selenium.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (C) 2015, MuChu Hsu
+Copyright (C) 2016, MuChu Hsu
 Contributed by Muchu Hsu (muchu1983@gmail.com)
 This file is part of BSD license
 
@@ -9,7 +9,7 @@ This file is part of BSD license
 import unittest
 import logging
 from selenium import webdriver
-
+from bennu.filesystemutility import FileSystemUtility
 """
 測試 Selenium
 """
@@ -19,21 +19,18 @@ class SeleniumTest(unittest.TestCase):
     #準備
     def setUp(self):
         logging.basicConfig(level=logging.INFO)
-        pass
-        
+        self.fileUtil = FileSystemUtility()
+        strChromeDriverPath = self.fileUtil.getPackageResourcePath(strPackageName="findfine_crawler.resource", strResourceName="chromedriver.exe")
+        self.driver = webdriver.Chrome(strChromeDriverPath)
     #收尾
     def tearDown(self):
-        pass
+        self.driver.quit()
 
     #測試 selenium
     def test_selenium(self):
-        chromedriver = ".\cameo_res\chromedriver.exe"
-        driver = webdriver.Chrome(chromedriver)
-        driver.get("https://www.indiegogo.com/explorer")
-        source = driver.page_source
-        #f = open("currency.html", "w+")
-        #f.write(source.encode("utf-8"))
-        #f.close()
+        logging.info("SeleniumTest.test_selenium")
+        self.driver.get("https://www.kkday.com/en/home")
+        source = self.driver.page_source
         
 #測試開始
 if __name__ == "__main__":
