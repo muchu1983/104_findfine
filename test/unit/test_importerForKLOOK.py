699a762de6523d3b08d53e8f294cbdb22e7e630c
MuChu Hsu <muchu1983@gmail.com>
2016-08-30 23:07:13 +0800

    add klook crawler and crawl index page okay

---
+++ test/unit/test_importerForKLOOK.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+import unittest
+import logging
+import json
+from findfine_crawler.importerForKKDAY import ImporterForKKDAY
+"""
+測試 KKDAY product.json 資料 import 至 DB
+"""
+class ImporterForKKDAYTest(unittest.TestCase):
+
+    #準備
+    def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+        self.importer = ImporterForKKDAY()
+        
+    #收尾
+    def tearDown(self):
+        pass
+    
+    #測試 import product.json to db
+    def test_import(self):
+        logging.info("ImporterForKKDAYTest.test_import")
+        self.importer.importProductJsonToDb()
+    
+#測試開始
+if __name__ == "__main__":
+    unittest.main(exit=False)
+
+
