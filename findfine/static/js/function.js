40ca24b1be5b93c1e51ea244f9afbc66c4329225
davidturtle <davidturtle0313@gmail.com>
2016-10-16 22:04:34 +0800

    1. 20161016進度_首頁程式編寫

---
+++ findfine/static/js/function.js
@@ -0,0 +1,38 @@
+
+
+// 飛機飛呀飛
+function planeFly(sec){
+    var ms = document.querySelectorAll('.flying_plane');
+    var defaultTiming = {
+        duration: sec,
+        iterations: 1,
+        fill: 'both',
+        easing: 'ease-in-out'
+    };
+
+    if (ms[0].style.motionOffset !== undefined) {
+        var easing = 'cubic-bezier(.645,.045,0.355,1)';
+
+        ms[0].animate([{
+            motionOffset: 0,
+            motionRotation: 'auto'
+        }, {
+            motionOffset: '100%',
+            motionRotation: 'auto'
+        }], defaultTiming);
+    } else {
+        document.documentElement.className = 'no-motionpath'
+    }
+}
+
+// 加至wishlist按鈕點擊
+function addToWishlistBtnClick(){
+    $(".add_wish_btn").click(function(event) {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        }
+        else{
+            $(this).addClass('active');
+        }
+    });
+}
