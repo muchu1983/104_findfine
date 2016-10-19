b99701e179dd5bec0dc21ac7b7e95183c2a12e28
josh <shiueih@gmail.com>
2016-10-19 18:11:14 +0800

    增加編輯內容 前端的部分

--- findfine/static/js/tripEdit.js
+++ findfine/static/js/tripEdit.js
@@ -2,17 +2,7 @@
     $(document).ready(initTripEdit);
     function initTripEdit(){
         
-        $('#external-events .fc-event').each(function() {
-            $(this).data('event', {
-                title: $.trim($(this).text()), 
-                stick: true 
-            });
-            $(this).draggable({
-                zIndex: 999,
-                revert: true,
-                revertDuration: 0
-            });
-        });
+
         
         $('#calendar').fullCalendar({
             header: {
@@ -30,6 +20,9 @@
         });
         
         //顯示wishList
+        //在执行之前加
+        $.ajaxSettings.async = false;
+        
         $.getJSON("/trip/getFavoriteTrip", function(jsonResp){
             $(".right ul").html("");
             var strUserCurrency = $("#moneySelect").val();
@@ -44,6 +37,33 @@
             };
 
         });
+        //执行你的代码之后及时恢复为
+        $.ajaxSettings.async = true;
+
+
+        
+        
+        $('.fc-event').each(function() {
+            alert($(this).text());
+            $(this).data('event', {
+                title: $.trim($(this).text()), 
+                stick: true 
+            });
+            $(this).draggable({
+                zIndex: 999,
+                revert: true,
+                revertDuration: 0
+            });
+            //將 trip 加入 my plans 中
+            //http://127.0.0.1:8000/trip/addTripPlanItem?intPlanId=3&strComment=%2277%22
+            
+            //var addTripPlanItem = "/trip/addTripPlanItem?intPlanId="+intId;
+            //$.getJSON(addTripPlanItem, function(jsonResp){
+            //    alert("jsonResp"+jsonResp);
+            //    });
+            
+        });
+        
         
         //組出單組查詢結果出來的html字串
         function getTripDataHtml(strUserCurrency, strTitle, intUserCurrencyCost, strIntroduction, strLocation, intDurationHour, strOriginUrl, strImageUrl, intReviewStar, intReviewVisitor, intId ){
@@ -66,15 +86,19 @@
             if(intReviewStar==5){
                 reviewStar='★★★★★';
             }
-    
-            var strIntroduction=strIntroduction.substr( 0 , 135 );
-    
+
+            
             var strTripDataHtml = [
             "<li id="+intId+" style=\"list-style-type:none;\">",
-                        "<div>",
+                        "<div class='fc-event'>",
                             "<p><img src=\""+strImageUrl+"\"/><p>",
                             "<p><span style=\"color:orange\">"+strTitle+"</span></p>",
                             "<p><a href="+strOriginUrl+" target=\"_blank\"> read more</a><p>",
+                            "<p><span> Duration:"+intDurationHour+"</span></p>",
+                            "<p><span> strUserCurrency:"+strUserCurrency+"</span></p>",
+                            "<p><span> intUserCurrencyCost:"+intUserCurrencyCost+"</span></p>",
+                            "<p><span> strLocation:"+strLocation+"</span></p>",
+                            "<p><span> strOriginUrl:"+strOriginUrl+"</span></p>",
                             "<p><span> Duration:"+intDurationHour+"</span></p>",
                             "<p><span style=\"color:red\">Stars:"+reviewStar+"</span></p>",
                             "<p><span>review:"+intReviewVisitor+"</span></p>",
