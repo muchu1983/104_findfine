7dc05d8016570b00b5b1008b859a3565b069a9ac
MuChu Hsu <muchu1983@gmail.com>
2016-11-09 02:22:04 +0800

    recommended trip tab 規劃

--- findfine/static/js/dashboard.js
+++ findfine/static/js/dashboard.js
@@ -2,7 +2,9 @@
     
     $(document).ready(initDashboard);
     
+    //初始化 dashboard
     function initDashboard(){
+        initRecommendedTrip();
         //nav tab
         $("#nav_tabs").tabs();
         $("#nav_tabs li").removeClass("ui-corner-top");
@@ -26,6 +28,45 @@
                 $("div.dashboardStatusDiv").html(jsonResp["config_status"])
             }, "json");
         });
+    };
+    
+    //初始化 推薦行程
+    function initRecommendedTrip(){
+        $("#searchTripBtn").click(function(){
+            searchKeyword();
+        });
+    };
+    
+    //搜尋含有 keyword 的 trip
+    function searchKeyword() {
+        //
+        var strKeyword = $("#keywordInput").val();
+        var strFilterQueryUrl = "/trip/filter?";
+        //keyword
+        if (strKeyword != "") {
+            strFilterQueryUrl = strFilterQueryUrl + "&keyword=" + strKeyword;
+        };
+        console.log(strFilterQueryUrl);
+        $.getJSON(strFilterQueryUrl, function(jsonResp) {
+            console.log(jsonResp);
+            //trip data
+            var lstDicTripData = jsonResp["trip"];
+            for (i = 0; i < lstDicTripData.length; i++) {
+                var dicTripData = lstDicTripData[i];
+                strTripOptionHtml = getTripOptionHtml(dicTripData["strTitle"], dicTripData["intId"]);
+                $("#searchResultSelect").append(strTripOptionHtml);
+            };
+        });
+    };
+    
+    //組成 select option 的 html 字串
+    function getTripOptionHtml(strTitle, intId){
+        var strTripOptionHtml = [
+            "<option value=\"" + intId + "\">",
+            strTitle,
+            "</option>"
+        ].join("");
+        return strTripOptionHtml;
     };
     
 })(jQuery);
