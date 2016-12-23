93f7f964cb8a865ad5a4eca5f39a571ac5ef79e4
josh <shiueih@gmail.com>
2016-12-24 06:12:10 +0800

    html+css

---
+++ findfine/static/js/contactUs.js
@@ -0,0 +1,124 @@
+//初始化autocomplate place_chenged event
+
+function initStatus() {
+
+    $('#myFriends').hide();
+    $('#myMessages').hide();
+    $('#logOut').hide();
+    $('#noLogHeadBtn').hide();
+    $("#headBtn").hide();
+    $(".login_btns").hide();
+    $(".logout_btns").hide();
+    //strEmail 如已登入 不顯示login button 並顯示會員帳號
+    if (strEmail == "None") {
+        // 暫時隱藏 為測試方便使用
+        $('#register').show();
+        $('#loginBtn').show();
+        $('#noLogHeadBtn').show();
+        $("#padRegister").show();
+        $("#padLoginBtn").show();
+        $(".logout_btns").show();
+    } else {
+        $(".login_btns").show();
+        $('#logOut').show();
+        $('#register').hide();
+        $('#loginBtn').hide();
+        $('#myFriends').show();
+        $('#myMessages').show();
+        $("#headBtn").show();
+        $("#padWishlist").show();
+        $("#padMyPlan").show();
+        $(".login_btns").show();
+    }
+}
+
+(function($) {
+
+    $(document).ready(initHome);
+
+    function initHome() {
+
+        // pad menu按鈕點擊
+        padMenuAct();
+
+        // mobile menu按鈕點擊
+        mobileMenuClick();
+
+        // toolbox點擊
+        toolboxClick()
+
+        homeInitCurrencySelect();
+
+
+        // 大LOGO箭頭動畫 @Q@davidturtle
+        $(".intro-text > .arrow_blk_hideblk>.arrow_blk").delay(1500).queue(function(next) {
+            $(this).addClass('active');
+            next();
+        });
+
+        // 頁面下滑選單效果 @Q@davidturtle
+        topNavDown(800);
+
+
+        // 頭像點擊
+        headBtnClick();
+
+        // 通知止滑
+        notiBlkPrevent();
+
+        initStatus();
+
+        // 登出動作
+        logoutToHome("#logOut");
+
+        //登入按鈕
+        $("#loginBtn").click(function() {
+            if ($("#loginBtn").html() == "Log In") {
+                window.location = "/account/login";
+            }
+        });
+
+        //申請按鈕
+        $("#register").click(function() {
+            if ($("#register").html() == "Sign Up") {
+                window.location = "/account/register";
+            }
+        });
+
+
+        // wishList按鈕點擊 @Q@ davidturtle
+        $("#wishList").click(function() {
+            window.location = "/page/wishList";
+        });
+
+        // pad版wishList按鈕點擊 @Q@ davidturtle
+        $("#padWishlist").click(function() {
+            window.location = "/page/wishList";
+        });
+
+        // myPlans按鈕點擊 @Q@ davidturtle
+        $("#myPlans").click(function() {
+            window.location = "/page/myPlan";
+        });
+
+        // pad版myPlans按鈕點擊 @Q@ davidturtle
+        $("#padMyPlan").click(function() {
+            window.location = "/page/myPlan";
+        });
+
+
+        //登入按鈕        
+        $("#loginBtn").click(function() {
+            window.location = "/account/login";
+        });
+
+        //未登入頭像按鈕        
+        $("#noLogHeadBtn").click(function() {
+            window.location = "/account/login";
+        });
+    };
+
+})(jQuery);
+
+
+
